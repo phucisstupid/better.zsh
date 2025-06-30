f2bd2e6a849d9c8d3017506c196cce4213f02e67
phucisstupid <125681538+phucisstupid@users.noreply.github.com>
2025-06-30 12:29:16 +0700

    Update navigation.plugin.zsh

--- navigation.plugin.zsh
+++ navigation.plugin.zsh
@@ -3,6 +3,37 @@ alias ff='fastfetch'
 alias of='onefetch'
 
 # ────────────────────────────────────────────────────────────
+# 🔌 Plugin Configurations
+# ────────────────────────────────────────────────────────────
+# 🌻 `zinit update`
+alias phucleeuwu='rm -rf ~/.local/share/zinit && exec zsh'
+
+# 🍺 `brew`
+export HOMEBREW_NO_ANALYTICS=1
+
+#  `eza` (Enhanced `ls`)
+zstyle ':omz:plugins:eza' 'show-group' no  
+zstyle ':omz:plugins:eza' 'git-status' yes  
+zstyle ':omz:plugins:eza' 'icons' yes  
+
+#  `alias-finder`
+zstyle ':omz:plugins:alias-finder' autoload yes  
+zstyle ':omz:plugins:alias-finder' longer yes  
+zstyle ':omz:plugins:alias-finder' exact yes  
+zstyle ':omz:plugins:alias-finder' cheaper yes  
+
+# `fzf` 
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
+
+# `magic-enter`
+MAGIC_ENTER_OTHER_COMMAND='ls -a .'
+
+# ────────────────────────────────────────────────────────────
 # 🔧 Shell Options - Smarter Navigation
 # ────────────────────────────────────────────────────────────
 
